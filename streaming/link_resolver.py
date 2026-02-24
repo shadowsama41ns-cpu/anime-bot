374ba71c0c5540796615801cdf9c004ade3a08fa
shadowsama41ns-cpu <shadowsama41ns@gmail.com>
2026-02-24 19:53:49 +0200

    Update link_resolver.py

--- streaming/link_resolver.py
+++ streaming/link_resolver.py
@@ -1,9 +1,5 @@
 from streaming.drive_resolver import resolve_drive_url as resolve_drive
 
 async def resolve_link(url: str):
-    """
-    Resolve qualquer link suportado (Drive, CDN etc)
-    """
-    # por enquanto apenas Drive
     final = await resolve_drive(url)
     return final
